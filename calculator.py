f2c5026f0f2902b00fc430153228968203c28537
uncle engineer <loong.wissawakorn@gmail.com>
2025-05-02 11:08:49 +0700

    add UncleEngineer

--- calculator.py
+++ calculator.py
@@ -8,7 +8,7 @@ GUI.geometry('500x700')
 L = Label(GUI,text='Loong Wissawakorn')
 L.pack()
 
-L = Label(GUI,text='your username')
+L = Label(GUI,text='Uncle Engineer')
 L.pack()
 
 L = Label(GUI,text='your username')
