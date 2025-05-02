580d037487e8dce419c9193d83842c28eaf448bb
Loong Wissawakorn <loong.wissawakorn3@gmail.com>
2025-05-02 10:43:33 +0700

    add calculator gui

---
+++ calculator.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import ttk, messagebox
+
+GUI = Tk()
+GUI.title('Basic Git & GitHub by Uncle Engineer')
+GUI.geometry('500x700')
+
+L = Label(GUI,text='Loong Wissawakorn')
+L.pack()
+
+L = Label(GUI,text='your username')
+L.pack()
+
+L = Label(GUI,text='your username')
+L.pack()
+
+L = Label(GUI,text='somchai_dev')
+L.pack()
+
+GUI.mainloop()
